cb43cca11c3c4979ca4cbe2d9ee402dbef95b0e6
OrtizGuilhermex <luiz_gf_ortiz@estudante.sesisenai.org.br>
2025-11-27 19:03:13 -0300

    feat: adiciona todos os testes de falaha funcionais

--- src/main/java/org/example/model/Falha.java
+++ src/main/java/org/example/model/Falha.java
@@ -5,19 +5,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class Falha {
-    /*CREATE TABLE IF NOT EXISTS Falha (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    equipamentoId BIGINT NOT NULL,
-    dataHoraOcorrencia DATETIME NOT NULL,
-    descricao TEXT NOT NULL,
-    criticidade VARCHAR(50) NOT NULL,
-    status VARCHAR(50) NOT NULL,
-    tempoParadaHoras DECIMAL(10, 2) DEFAULT 0.00,
 
-    -- Garante que criticidade e status tenham valores pré-definidos
-    CONSTRAINT chk_criticidade_falha CHECK (criticidade IN ('BAIXA', 'MEDIA', 'ALTA', 'CRITICA')),
-    CONSTRAINT chk_status_falha CHECK (status IN ('ABERTA', 'EM_ANDAMENTO', 'RESOLVIDA')),
-*/
     private Long id;
 
     private Long equipamentoId;
