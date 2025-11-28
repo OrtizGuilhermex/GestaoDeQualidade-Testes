ef667191319957818c11fe8210667db32f63a308
OrtizGuilhermex <luiz_gf_ortiz@estudante.sesisenai.org.br>
2025-11-27 21:48:03 -0300

    feat: Adiciona metodo para buscarEquipamentosSemFalhasPorPeriodo

--- src/main/java/org/example/repository/EquipamentoRepository.java
+++ src/main/java/org/example/repository/EquipamentoRepository.java
@@ -2,9 +2,11 @@ package org.example.repository;
 
 import org.example.database.Conexao;
 import org.example.model.Equipamento;
-import org.example.model.Falha;
 
 import java.sql.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class EquipamentoRepository {
 
@@ -14,16 +16,16 @@ public class EquipamentoRepository {
                 """;
 
         try (Connection conn = Conexao.conectar();
-             PreparedStatement stmt = conn.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)){
+             PreparedStatement stmt = conn.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
 
             stmt.setString(1, equipamento.getNome());
             stmt.setString(2, equipamento.getNumeroDeSerie());
-            stmt.setString(3,equipamento.getAreaSetor());
+            stmt.setString(3, equipamento.getAreaSetor());
             stmt.setString(4, "OPERACIONAL");
             stmt.executeUpdate();
 
             ResultSet rs = stmt.getGeneratedKeys();
-            if(rs.next()){
+            if (rs.next()) {
                 Long id = rs.getLong(1);
                 equipamento.setId(id);
             }
@@ -31,7 +33,7 @@ public class EquipamentoRepository {
         return equipamento;
     }
 
-    public Equipamento buscarEquipamento (Long id) throws  SQLException{
+    public Equipamento buscarEquipamento(Long id) throws SQLException {
         String query = """
                 SELECT id
                 ,nome
@@ -48,16 +50,52 @@ public class EquipamentoRepository {
             stmt.setLong(1, id);
             ResultSet rs = stmt.executeQuery();
 
-            if(rs.next()){
+            if (rs.next()) {
                 return new Equipamento(
                         rs.getLong("id"),
                         rs.getString("nome"),
                         rs.getString("numeroDeSerie"),
                         rs.getString("areaSetor"),
                         rs.getString("statusOperacional")
-                        );
+                );
             }
         }
         return null;
+    }
+
+    public List<Equipamento> buscarEquipamentosSemFalhasPorPeriodo(LocalDate dataInicio, LocalDate dataFim) throws SQLException {
+        List<Equipamento> equipamentos = new ArrayList<>();
+
+        String query = """
+                SELECT id
+                ,nome
+                ,numeroDeSerie
+                ,areaSetor
+                ,statusOperacional
+                FROM Equipamento
+                WHERE id NOT IN
+                (SELECT equipamentoId
+                FROM Falha
+                WHERE dataHoraOcorrencia >= ? AND dataHoraOcorrencia <= ?)
+                """;
+
+        try (Connection conn = Conexao.conectar();
+             PreparedStatement stmt = conn.prepareStatement(query)) {
+            stmt.setDate(1, Date.valueOf(dataInicio));
+            stmt.setDate(2, Date.valueOf(dataFim));
+
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                long id = rs.getLong("id");
+                String nome = rs.getString("nome");
+                String numeroDeSerie = rs.getString("numeroDeSerie");
+                String areaSetor = rs.getString("areaSetor");
+                String statusOperacional = rs.getString("statusOperacional");
+                Equipamento equipamento = new Equipamento(id,nome,numeroDeSerie,areaSetor,statusOperacional);
+                equipamentos.add(equipamento);
+            }
+        }
+        return equipamentos;
     }
 }
