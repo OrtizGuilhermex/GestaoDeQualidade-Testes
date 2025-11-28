7b90bd1b275ef6f89c221d2ea8e04415711554e4
OrtizGuilhermex <luiz_gf_ortiz@estudante.sesisenai.org.br>
2025-11-27 21:48:59 -0300

    feat: Adiciona implementacoes para gerar relatorios

--- src/main/java/org/example/service/relatorioservice/RelatorioServiceImpl.java
+++ src/main/java/org/example/service/relatorioservice/RelatorioServiceImpl.java
@@ -4,6 +4,10 @@ import org.example.dto.EquipamentoContagemFalhasDTO;
 import org.example.dto.FalhaDetalhadaDTO;
 import org.example.dto.RelatorioParadaDTO;
 import org.example.model.Equipamento;
+import org.example.model.Falha;
+import org.example.repository.AcaoCorretivaRepository;
+import org.example.repository.EquipamentoRepository;
+import org.example.repository.FalhaRepository;
 
 import java.sql.SQLException;
 import java.time.LocalDate;
@@ -11,19 +15,36 @@ import java.util.List;
 import java.util.Optional;
 
 public class RelatorioServiceImpl implements RelatorioService{
+    FalhaRepository falhaRepository = new FalhaRepository();
+    EquipamentoRepository equipamentoRepository = new EquipamentoRepository();
+    AcaoCorretivaRepository acaoCorretivaRepository = new AcaoCorretivaRepository();
+
     @Override
     public List<RelatorioParadaDTO> gerarRelatorioTempoParada() throws SQLException {
-        return List.of();
+        return falhaRepository.gerarRelatorioTempoParada();
     }
 
     @Override
     public List<Equipamento> buscarEquipamentosSemFalhasPorPeriodo(LocalDate dataInicio, LocalDate datafim) throws SQLException {
-        return List.of();
+        return equipamentoRepository.buscarEquipamentosSemFalhasPorPeriodo(dataInicio,datafim);
     }
 
     @Override
     public Optional<FalhaDetalhadaDTO> buscarDetalhesCompletosFalha(long falhaId) throws SQLException {
-        return Optional.empty();
+        Falha falha = falhaRepository.buscarFalhaPorId(falhaId);
+
+        if(falha == null){
+            throw new RuntimeException();
+        }
+
+        Equipamento equipamento = equipamentoRepository.buscarEquipamento(falha.getEquipamentoId());
+
+        if(equipamento == null){
+            throw new RuntimeException();
+        }
+
+        List<String> acao = acaoCorretivaRepository.buscarAcaoCorretivaPorIdFalha(falhaId);
+        return Optional.of(new FalhaDetalhadaDTO(falha,equipamento,acao));
     }
 
     @Override
